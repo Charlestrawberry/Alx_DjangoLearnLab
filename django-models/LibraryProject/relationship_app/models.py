972f6492ab12628671fe2cea2bfb467fab606c66
Oduks <charlestrawb_odus@yahoo.com>
2025-09-07 01:52:46 +0100

    created a model app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
 
 # one to many
 class Book(models.Model):
