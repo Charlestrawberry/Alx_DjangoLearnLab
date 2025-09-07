771acc9f2b5741146d1cfde82ef6b5987e57e934
Oduks <charlestrawb_odus@yahoo.com>
2025-09-07 22:42:26 +0100

    created a auth view, userprofile for diff members

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,6 +10,14 @@ urlpatterns = [
 
    # Authentication
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
     path('register/', views.register, name='register'),
+
+    path('admin-view/', views.admin_view, name='admin_view'),
+
+    path('librarian-view/', views.librarian_view, name='admin_view'),
+
+    path('member-view/', views.member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
-
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
 
 from django.contrib.auth.views import LoginView, LogoutView
@@ -27,7 +27,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # ✅ log in the user right after registration
+            login(request, user)  #  log in the user right after registration
             return redirect("list_books")
     else:
         form = UserCreationForm()
@@ -42,3 +42,25 @@ class LoginView(LoginView):
 # User Logout (class-based view using built-in)
 class LogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
