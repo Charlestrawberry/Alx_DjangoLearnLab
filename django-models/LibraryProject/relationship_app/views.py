ab23da75efcd2d728c9768610017cfb04bf75b6d
Oduks <charlestrawb_odus@yahoo.com>
2025-09-07 03:40:04 +0100

    created templates and auth

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
 from django.views.generic.detail import DetailView
 # Create your views here.
 
@@ -14,3 +17,24 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
+
+# User Registration
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")  # redirect to login after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
+
+
+# User Login (class-based view using built-in)
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+
+
+# User Logout (class-based view using built-in)
+class CustomLogoutView(LogoutView):
+    template_name = "relationship_app/logout.html"
