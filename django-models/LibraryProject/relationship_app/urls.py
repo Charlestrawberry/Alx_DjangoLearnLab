400460754286742a96a917f1dfa2c9c93da5badc
Oduks <charlestrawb_odus@yahoo.com>
2025-09-08 00:02:37 +0100

    implemented permissions

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,9 +21,14 @@ urlpatterns = [
 
     path('member-view/', views.member_view, name='member_view'),
 
+
+
     path('books/add/', views.add_book, name='add_book'),
 
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book')
+
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
     
+     # (Optional) list all books
+    path('books/', views.book_list, name='book_list'),
 ]
