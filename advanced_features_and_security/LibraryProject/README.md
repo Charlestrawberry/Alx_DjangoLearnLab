7e470a96f4d0190c40b63460b9c0ee2a0e50cd3f
Oduks <charlestrawb_odus@yahoo.com>
2025-09-21 23:33:19 +0100

    created a user auth

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,1 +1,14 @@
 Learning to create django project 
+
+"""
+Permissions Setup:
+- can_view: Allows viewing ExampleModel.
+- can_create: Allows creating ExampleModel.
+- can_edit: Allows editing ExampleModel.
+- can_delete: Allows deleting ExampleModel.
+
+Groups:
+- Viewers → can_view
+- Editors → can_view, can_create, can_edit
+- Admins → all permissions
+"""
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def example_list(request):
+    return render(request, "bookshelf/example_list.html")
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def example_create(request):
+    return render(request, "bookshelf/example_create.html")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def example_edit(request, pk):
+    return render(request, "bookshelf/example_edit.html")
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def example_delete(request, pk):
+    return render(request, "bookshelf/example_delete.html")
