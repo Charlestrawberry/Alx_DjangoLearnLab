f6e913747e5d8de3607dc33fc97a9f9d35e9001d
Oduks <charlestrawb_odus@yahoo.com>
2025-09-21 23:41:28 +0100

    created a user auth

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,17 +2,17 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def example_list(request):
-    return render(request, "bookshelf/example_list.html")
+def book_list(request):
+    return render(request, "bookshelf/book_list.html, {'books': books}")
 
 @permission_required('bookshelf.can_create', raise_exception=True)
-def example_create(request):
-    return render(request, "bookshelf/example_create.html")
+def book_create(request):
+    return render(request, "bookshelf/book_create.html")
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def example_edit(request, pk):
-    return render(request, "bookshelf/example_edit.html")
+def book_edit(request, pk):
+    return render(request, "bookshelf/book_edit.html")
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def example_delete(request, pk):
-    return render(request, "bookshelf/example_delete.html")
+def book_delete(request, pk):
+    return render(request, "bookshelf/book_delete.html")
