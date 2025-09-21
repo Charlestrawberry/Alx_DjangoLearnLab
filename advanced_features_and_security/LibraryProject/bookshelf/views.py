70760abfd8592815ea4c9cb8e7507d986880e1f3
Oduks <charlestrawb_odus@yahoo.com>
2025-09-22 00:07:28 +0100

    created a user auth

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 
 def create_book(request):
     if request.method == "POST":
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("book_list")  # redirect to book list after saving
     else:
-        form = BookForm()
+        form = ExampleForm()
     
     return render(request, "bookshelf/form-example.html", {"form": form})
 
