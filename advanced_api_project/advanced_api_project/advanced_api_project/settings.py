405572024f5599e112a9cdac3c70a6e1bc63de2f
Oduks <charlestrawb_odus@yahoo.com>
2025-09-28 21:54:03 +0100

    created models, serializers and custom views

--- advanced_api_project/advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/advanced_api_project/settings.py
@@ -48,6 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
 }
